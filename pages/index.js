d1c7559a0b5229d9f326cac135c1ac396cb4ce3b
Spunge01 <mrinal.dxzt23@gmail.com>
2025-04-11 18:35:03 +0530

    fix: resolved router conflict

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ fontFamily: "sans-serif", padding: "2rem" }}>
-      <h1>Welcome to My Next.js Site</h1>
-      <p>If you're seeing this, it works!</p>
-    </div>
-  );
-}
